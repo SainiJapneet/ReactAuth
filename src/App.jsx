cfebfbbccc42216551f7612efb07becf72b11351
japneet <japneetsaini084@gmail.com>
2023-06-26 09:54:22 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {Account} from './Account';
+import { Container } from 'react-bootstrap';
+function App() {
+
+  return (
+    <Container>
+      <Account/>
+    </Container>
+  )
+}
+
+export default App
